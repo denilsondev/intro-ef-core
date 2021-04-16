97a9d92ec443e50f361abfc938ff3b53855582f0
denilsondev <denilsoncdc10@gmail.com>
2021-04-16 11:28:22 -0300

    deat: add migration

--- Curso/Data/Configurations/PedidoConfiguration.cs
+++ Curso/Data/Configurations/PedidoConfiguration.cs
@@ -10,7 +10,7 @@ namespace CursoEfCore.Data.Configurations
         {
               builder.ToTable("Pedidos");
                 builder.HasKey(p => p.Id);
-                builder.Property(p => p.iniciadoEm).HasDefaultValue("GETDATE").ValueGeneratedOnAdd();
+                builder.Property(p => p.IniciadoEm).HasDefaultValueSql("GETDATE()").ValueGeneratedOnAdd();
                 builder.Property(p => p.Status).HasConversion<string>();
                 builder.Property(p => p.TipoFrete).HasConversion<int>();
                 builder.Property(p => p.Observacao).HasColumnType("VARCHAR(512)");
--- Curso/Domain/Pedido.cs
+++ Curso/Domain/Pedido.cs
@@ -8,8 +8,8 @@ namespace CursoEfCore.Domain {
         public int Id { get; set; }
         public int ClienteId { get; set; }
         public Cliente Cliente { get; set; }
-        public DateTime iniciadoEm { get; set; }
-        public DateTime finalizadoEm { get; set; }
+        public DateTime IniciadoEm { get; set; }
+        public DateTime FinalizadoEm { get; set; }
         public TipoFrete TipoFrete { get; set; }
         public StatusPedido Status { get; set; }
         public string Observacao { get; set; }
